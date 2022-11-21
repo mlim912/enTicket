ebb6710b841a37a4bba710f898f16eeb1efa18e8
Molika Lim <limmolika99@gmail.com>
2022-11-21 22:01:58 +1300

    second deployment

--- Program.cs
+++ Program.cs
@@ -63,7 +63,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Movies}/{action=Index}/{id?}");
 
 //Seed database
 AppDbinitializer.Seed(app);
